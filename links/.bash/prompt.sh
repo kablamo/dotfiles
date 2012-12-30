bdf76210ffb049f600c8e4255640bebf07bec1e5
Eric Johnson <eric.git@iijo.org>
2012-12-30 22:51:51 +0000

    prompt now has my git status on it.

--- links/.bash/prompt.sh
+++ links/.bash/prompt.sh
@@ -1,7 +1,54 @@
 
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
+## set variable identifying the chroot you work in (used in the prompt below)
+#if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+#    debian_chroot=$(cat /etc/debian_chroot)
+#fi
+#
+#PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w ⚡ '
+#
 
-PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w ⚡ '
+
+function __git_prompt {
+  GIT_PS1_SHOWDIRTYSTATE=1
+  __git_ps1 "[%s] " | sed 's/ \([+*]\{1,\}\)] $/\1] /'
+}
+
+# Only show username@server over SSH.
+function __name_and_server {
+  if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+    echo "`whoami`@`hostname -s`:"
+  fi
+}
+
+bash_prompt() {
+
+  # regular colors
+  local K="\[\033[0;30m\]"    # black
+  local R="\[\033[0;31m\]"    # red
+  local G="\[\033[0;32m\]"    # green
+  local Y="\[\033[0;33m\]"    # yellow
+  local B="\[\033[0;34m\]"    # blue
+  local M="\[\033[0;35m\]"    # magenta
+  local C="\[\033[0;36m\]"    # cyan
+  local W="\[\033[0;37m\]"    # white
+
+  # emphasized (bolded) colors
+  local BK="\[\033[1;30m\]"
+  local BR="\[\033[1;31m\]"
+  local BG="\[\033[1;32m\]"
+  local BY="\[\033[1;33m\]"
+  local BB="\[\033[1;34m\]"
+  local BM="\[\033[1;35m\]"
+  local BC="\[\033[1;36m\]"
+  local BW="\[\033[1;37m\]"
+
+  # reset
+  local RESET="\[\033[0;37m\]"
+
+  PS1="$BW\$(__name_and_server)$RESET\w \$(__git_prompt)⚡ "
+
+}
+
+bash_prompt
+unset bash_prompt
+
