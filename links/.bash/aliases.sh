27863aa0019987f94bf958c9c1c24386f154d4e6
Eric Johnson <eric.git@iijo.org>
2013-01-04 11:22:46 +0000

    .

--- links/.bash/aliases.sh
+++ links/.bash/aliases.sh
@@ -15,7 +15,7 @@ alias -- -='cd -'
 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 alias ack='ack --perl --ignore-dir=t -A 5'
-alias irc='ssh eric@braga.cuckoo.org -R 7877:localhost:7877'
+alias irc='ssh eric@braga -R 7877:localhost:7877'
 alias tunnel='autossh -v -R 9999:localhost:9999 -N -l eric iijo.org -p 4321'
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 alias t='/home/eric/code/dotfiles/bin/todo.sh -p -P'
@@ -28,7 +28,7 @@ alias upgrade='sudo apt-get upgrade'
 alias search="apt-cache search"
 
 alias minicpanm='cpanm --mirror ~/minicpan --mirror-only'
-alias darkpan='cpanm --mirror http://mycompany.foxtons.co.uk/DPAN --mirror-only'
+alias darkpan='cpanm --mirror http://mycompany.co.uk/DPAN --mirror-only'
 alias pod='perlfind'
 
 alias GET='lwp-request -m GET '
