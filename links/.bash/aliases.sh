15a4065fdd39cc4d0a6e9b6005e9f2b88734e5e4
Eric Johnson <eric.git@iijo.org>
2013-01-06 00:43:12 +0000

    Added translate(), say(), and tsay().

--- links/.bash/aliases.sh
+++ links/.bash/aliases.sh
@@ -1,6 +1,41 @@
 wiki() {
-   dig +short txt $1.wp.dg.cx
+    dig +short txt $1.wp.dg.cx
 }
+
+say() { 
+    tl=$1
+    shift 
+    if [ "$1" = "-" ] ; then
+        read text
+        echo $text
+    else
+        text=$*
+    fi
+    text=$(echo $text | sed 's#\ #\+#g')
+    mplayer -user-agent Mozilla "http://translate.google.com/translate_tts?tl=$tl&ie=UTF-8&q=$text" > /dev/null 2>&1 
+}
+
+translate() {
+    sl=$1
+    tl=$2
+    shift
+    shift
+    text=$(echo $* | sed 's#\ #\+#g')
+    len=`expr length "$text"`
+    if [ -z "$text" ] ; then
+        echo "Try again small friend"
+        exit 4
+    elif [ "$len" -gt "100" ] ; then
+        echo "Cannot translate more than 100 characters at once ($len)"
+        exit 2
+    fi
+    wget -U "Mozilla/5.0" -qO - "http://translate.google.com/translate_a/t?client=t&text=$text&sl=$sl&tl=$tl" | sed 's/\[\[\[\"//' | cut -d \" -f 1;
+}
+
+tsay() {
+    translate $* | say $2 -
+}
+
 
 alias hg='history | grep --color=auto'
 alias grep='grep --color=auto'
