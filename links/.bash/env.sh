49d7af1252d7040ca31fffece641aaecf5ac9c01
Eric Johnson <eric.git@iijo.org>
2013-01-12 20:55:45 +0000

    Setup env for perlbrew.

--- links/.bash/env.sh
+++ links/.bash/env.sh
@@ -8,16 +8,11 @@ export LESS="-MSiRXF"
 # TODO: delete this
 export PAGER="/usr/bin/less"
 
-# local::lib
-#export PERL_LOCAL_LIB_ROOT="/home/eric/perl5";
-#export PERL_MB_OPT="--install_base $HOME/perl5"
-#export PERL_MM_OPT="INSTALL_BASE=$HOME/perl5"
-#export PERL5LIB="$HOME/perl5/lib/perl5/i686-linux:$HOME/perl5/lib/perl5/i686-linux/auto:$HOME/perl5/lib/perl5/i386-linux:$HOME/perl5/lib/perl5/i686-linux-gnu-thread-multi-64int:$HOME/perl5/lib/perl5/i686-linux-gnu-thread-multi-64int/auto:$HOME/perl5/lib/perl5"
-export PERL5LIB=.:./lib:$PERL5LIB
 
 # perlbrew
-export PERLBREW_ROOT=/home/eric/perl5/perlbrew
+export PERLBREW_ROOT="$HOME/perl5/perlbrew"
 [ -x $PERLBREW_ROOT ] && source ${PERLBREW_ROOT}/etc/bashrc
+[ -x $PERLBREW_ROOT ] && export PERL_CPANM_OPT="--local-lib=$PERLBREW_ROOT/perls/perl-5.16.2"
 
-# cpanm
-#export PERL_CPANM_OPT="--local-lib=~/perl5"
+# PERL5LIB
+export PERL5LIB=.:./lib:$PERL5LIB:$PERLBREW_ROOT/perls/perl-5.16.2/lib/perl5:$PERLBREW_ROOT/perls/perl-5.16.2/lib/5.16.2
